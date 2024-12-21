699794d3369248fe2308b2c2e8b3a99d7a4d1573
banana4560 <andy0916903551@gmail.com>
2024-12-21 17:20:48 +0800

    20241221

--- server.js
+++ server.js
@@ -11,24 +11,17 @@ var ProfolioDB = DB.create(__dirname + "/profolio.db"); // 創建資料庫實例
 // 使用 Express 內建的靜態檔案中介軟體提供靜態資源
 server.use(express.static(__dirname + "/banana"));
 
-// 設定作品集的 API 路由
-server.get("/profolio", (req, res) => {
-  // 從資料庫中查詢所有作品集資料
-  ProfolioDB.find({}).then(results => {
-    if (results.length > 0) {
-      // 如果資料存在，回傳 JSON 格式的資料
-      res.json(results);
-    } else {
-      // 如果沒有資料，回傳錯誤訊息
-      res.status(404).send("No data found!");
+server.get("/banana/index", (req,res)=>{
+  //DB
+  ProfolioDB.find({}).then(results=>{
+    if(results != null){
+         res.send(results);
+    }else{
+        res.send("Error!");
     }
-  }).catch(err => {
-    // 錯誤處理
-    res.status(500).send("Error fetching data from database!");
-  });
-});
+  })
+})
 
-// 設定連接的端口
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+server.listen(80, ()=>{
+  console.log("Server is running at port 80.");
+})
